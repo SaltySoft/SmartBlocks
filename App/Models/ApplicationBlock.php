1f0e5750e38e10e768ef7c5bceab85a525e844f6
Antoine Jackson <a.j.william26@gmail.com>
2013-03-15 17:33:07 +0100

    * Added application and applicationblock models

---
+++ App/Models/ApplicationBlock.php
@@ -0,0 +1,76 @@
+<?php
+/**
+ * Date: 15/03/2013
+ * Time: 16:28
+ * This is the model class called ApplicationBlock
+ */
+
+/**
+ * @Entity @Table(name="applicationblocks")
+ */
+class ApplicationBlock extends Model
+{
+    /**
+     * @Id @GeneratedValue(strategy="AUTO") @Column(type="integer")
+     */
+    public $id;
+
+    /**
+     * @Column(type="string")
+     */
+    private $name;
+
+    /**
+     * @Column(type="text")
+     */
+    private $description;
+
+    /**
+     * @OneToMany(targetEntity="Application", mappedBy="block")
+     */
+    private $applications;
+
+    public function addApplications($application)
+    {
+        $this->applications[] = $application;
+    }
+
+    public function removeApplication($application)
+    {
+        $this->applications->removeElement($application);
+    }
+
+    public function getApplications()
+    {
+        return $this->applications;
+    }
+
+    public function setDescription($description)
+    {
+        $this->description = $description;
+    }
+
+    public function getDescription()
+    {
+        return $this->description;
+    }
+
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    public function setName($name)
+    {
+        $this->name = $name;
+    }
+
+    public function getName()
+    {
+        return $this->name;
+    }
+
+
+
+}
+
