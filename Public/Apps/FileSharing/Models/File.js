9e60dd6739ddf846cdd50e6010cbe55f6d9087a5
Antoine Jackson <a.j.william26@gmail.com>
2013-04-29 22:44:38 +0200

    * Schemas & Files : it is now possible to export schema to image file with * text

--- Public/Apps/FileSharing/Models/File.js
+++ Public/Apps/FileSharing/Models/File.js
@@ -1,20 +1,34 @@
 define([
     'underscore',
     'backbone'
-], function (_, Backbone){
-   var File = Backbone.Model.extend({
-       urlRoot: "/Files",
-       defaults: {
-       },
-       parse : function (response) {
+], function (_, Backbone) {
+    var File = Backbone.Model.extend({
+        urlRoot: "/Files",
+        defaults: {
+        },
+        parse: function (response) {
 
-           var parent_f_object = response.parent_folder;
-           var parent_folder = new File(parent_f_object);
-           response.parent_folder = parent_folder;
-           console.log(response);
-           return response;
-       }
-   });
+            var parent_f_object = response.parent_folder;
+            var parent_folder = new File(parent_f_object);
+            response.parent_folder = parent_folder;
+
+            var subfiles = response.subfiles;
+            var subfiles_collection = new FilesCollection();
+            for (var k in subfiles) {
+                var file = new File(subfiles[k]);
+                subfiles_collection.add(file);
+            }
+
+            response.subfiles = subfiles_collection;
+            console.log(response);
+            return response;
+        }
+    });
+
+    var FilesCollection = Backbone.Collection.extend({
+        model: File,
+        url: "/Files"
+    });
 
     return File;
 });
