c06cb55af4f78c63394a98be754f9e5415de857e
Vincent Lefebvre <vinc.lefebv@gmail.com>
2013-05-01 15:01:49 +0200

    * merging

--- Public/Apps/Common/Views/TextEditor.js
+++ Public/Apps/Common/Views/TextEditor.js
@@ -86,8 +86,6 @@ define([
                 base.events.trigger('text_editor_blur', message);
             });
 
-
-
             frame.contents().delegate("body", "keyup", function (e) {
                 base.resizeFrame();
                 base.events.trigger("text_editor_keyup", e.keyCode);
