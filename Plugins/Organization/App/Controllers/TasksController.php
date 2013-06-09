97e9a11f4b75423e23eab488178f29c19aec0e06
Cedric Eugeni <cedric.eugeni@3ie.fr>
2013-06-09 16:13:35 +0200

    added interop iPad on tasks

--- Plugins/Organization/App/Controllers/TasksController.php
+++ Plugins/Organization/App/Controllers/TasksController.php
@@ -4,20 +4,59 @@ namespace Organization;
 
 class TasksController extends \Controller
 {
+    public function security_check()
+    {
+        if (!\User::logged_in())
+        {
+            $this->redirect("/Meetings/Schemas/error");
+        }
+    }
 
+    public function security_check_token($token)
+    {
+        $users = \User::where(array("token" => $token));
+        if ($users != null && count($users) == 0)
+        {
+            $this->redirect("Meetings/Schemas/error");
+        }
+    }
+
+    public function error()
+    {
+        $this->render = false;
+        header("Content-Type: application/json");
+        echo json_encode(array("status" => "error", "message" => "You are not logged in"));
+    }
 
     public function index()
     {
+        if (isset($_GET["token"]) && $_GET["token"] != "")
+            $this->security_check_token($_GET["token"]);
+        else
+            $this->security_check();
+
         $em = \Model::getEntityManager();
 
         $qb = $em->createQueryBuilder();
 
-        $qb->select("t")
-            ->from("\\Organization\\Task", "t")
-            ->leftJoin("t.linked_users", "tu")
-            ->where("t.owner = :user OR (tu.user = :user)")
-            ->setParameter("user", \User::current_user())
-            ->orderBy("t.due_date");
+        if (\User::current_user() != null)
+            $qb->select("t")
+                ->from("\\Organization\\Task", "t")
+                ->leftJoin("t.linked_users", "tu")
+                ->where("t.owner = :user OR (tu.user = :user)")
+                ->setParameter("user", \User::current_user())
+                ->orderBy("t.due_date");
+        else
+        {
+            $users = \User::where(array("token" => $_GET["token"]));
+            $user = $users[0];
+            $qb->select("t")
+                ->from("\\Organization\\Task", "t")
+                ->leftJoin("t.linked_users", "tu")
+                ->where("t.owner = :user OR (tu.user = :user)")
+                ->setParameter("user", $user)
+                ->orderBy("t.due_date");
+        }
 
         $results = $qb->getQuery()->getResult();
 
@@ -33,12 +72,25 @@ class TasksController extends \Controller
 
     public function create()
     {
+        if (isset($_GET["token"]) && $_GET["token"] != "")
+            $this->security_check_token($_GET["token"]);
+        else
+            $this->security_check();
+
         $data = $this->getRequestData();
 
         $task = new Task;
 
         $task->setName($data["name"]);
         $task->setDueDate($data["due_date"]);
+
+        if (isset($_GET["token"]) && $_GET["token"] != "")
+        {
+            $users = \User::where(array("token" => $_GET["token"]));
+            $user = $users[0];
+            $task->setOwner($user);
+        }
+
         $task->save();
 
         $this->render = false;
@@ -48,6 +100,11 @@ class TasksController extends \Controller
 
     public function show($params = array())
     {
+        if (isset($_GET["token"]) && $_GET["token"] != "")
+            $this->security_check_token($_GET["token"]);
+        else
+            $this->security_check();
+
         $task = Task::find($params["id"]);
         $this->render = false;
         header("Content-Type: application/json");
@@ -63,6 +120,11 @@ class TasksController extends \Controller
 
     public function update($params = array())
     {
+        if (isset($_GET["token"]) && $_GET["token"] != "")
+            $this->security_check_token($_GET["token"]);
+        else
+            $this->security_check();
+
         $task = Task::find($params["id"]);
         $this->render = false;
         header("Content-Type: application/json");
@@ -109,6 +171,11 @@ class TasksController extends \Controller
 
     public function destroy($params = array())
     {
+        if (isset($_GET["token"]) && $_GET["token"] != "")
+            $this->security_check_token($_GET["token"]);
+        else
+            $this->security_check();
+
         $this->render = false;
         header("Content-Type: application/json");
         $task = Task::find($params["id"]);
