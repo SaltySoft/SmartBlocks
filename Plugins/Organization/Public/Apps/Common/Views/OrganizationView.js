762e4a0acb15e5585511c75529a73152a278b16a
Antoine Jackson <a.j.william26@gmail.com>
2013-06-10 23:53:18 +0200

    * Small tweak in blocks config (organization)

--- Plugins/Organization/Public/Apps/Common/Views/OrganizationView.js
+++ Plugins/Organization/Public/Apps/Common/Views/OrganizationView.js
@@ -34,7 +34,6 @@ define([
                     base.launchCalendar();
                 },
                 daily: function () {
-                    console.log("asdasd");
                     base.launchPlanning();
                 }
             });
