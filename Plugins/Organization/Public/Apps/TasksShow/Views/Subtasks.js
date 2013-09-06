08325b8979282f239273a11ee15f867a58fe59f3
Vincent Lefebvre <vinc.lefebv@gmail.com>
2013-09-06 19:33:46 +0200

    * upgrade table edition and display.

--- Plugins/Organization/Public/Apps/TasksShow/Views/Subtasks.js
+++ Plugins/Organization/Public/Apps/TasksShow/Views/Subtasks.js
@@ -55,7 +55,40 @@ define([
             });
 
             base.$el.delegate(".subtask_infos", "click", function () {
+                var elt = $(this);
                 console.log("click subtask_infos");
+                var subtask_id = elt.attr("data-id");
+                console.log("subtask_id", subtask_id);
+
+                base.$el.find(".subtask_show_container").html('<div >' + 'Description:' + elt.attr("data-description") + '</div >');
+            });
+
+            base.$el.delegate(".subtask_button", "click", function () {
+                var elt = $(this);
+                var action = elt.attr("data-action");
+                var subtask_infos = elt.closest(".subtask_infos");
+                var subtask_id = subtask_infos.attr("data-id");
+                var subtask = base.task.get('subtasks').get(subtask_id);
+
+                if (action == "edit") {
+                    subtask_infos.addClass("edition");
+                    subtask_infos.find(".subtask_display").each(function () {
+                        var elt = $(this);
+                        var closest_input = elt.closest("");
+                        console.log("subtask_display1", elt.html());
+
+                    });
+                }
+                if (action == "save") {
+
+                }
+                if (action == "cancel") {
+                    subtask_infos.removeClass("edition");
+                }
+                if (action == "delete") {
+
+                }
+
             });
 
 
@@ -63,7 +96,7 @@ define([
                 $(this).addClass('table-line-hover');
             }, function () {
                 $(this).removeClass('table-line-hover');
-            });
+            })
         }
     });
 
