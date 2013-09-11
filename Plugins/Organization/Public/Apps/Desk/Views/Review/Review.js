54977462062b81ad4d3402b27c1f2b0d5fb1eee6
Antoine Jackson <a.j.william26@gmail.com>
2013-09-11 19:41:22 +0200

    * Organization - desk/review pagination + left explain panel

--- Plugins/Organization/Public/Apps/Desk/Views/Review/Review.js
+++ Plugins/Organization/Public/Apps/Desk/Views/Review/Review.js
@@ -17,9 +17,9 @@ define([
                 for (var k in planned_tasks.models) {
                     var pt = planned_tasks.models[k];
                     var start = pt.getStart();
-                    start.setHours(0,0,0,0);
+                    start.setHours(0, 0, 0, 0);
                     var stop = pt.getStop();
-                    stop.setHours(23,59,59,999);
+                    stop.setHours(23, 59, 59, 999);
                     if (start < now && now < stop) {
                         display = true;
                     }
@@ -29,6 +29,7 @@ define([
 
             base.current_page = 1;
             base.page_size = 10;
+            base.page_count = 1;
         },
         init: function (SmartBlocks) {
             var base = this;
@@ -43,15 +44,37 @@ define([
             var template = _.template(review_template, {});
             base.$el.html(template);
 
+            var total_count = base.tasks.models.length;
+            base.page_count = Math.ceil(total_count / base.page_size);
 
-            for (var k in base.tasks.models) {
+            if (base.current_page < 1) {
+                base.current_page = 1;
+            } else if (base.current_page > base.page_count) {
+                base.current_page = base.page_count;
+            }
+
+            var page_start = (base.current_page - 1) * base.page_count;
+            var page_stop = page_start + base.page_count + 1;
+
+            current_list = base.tasks.slice(page_start, page_stop);
+
+            for (var k in current_list) {
                 var thumbnail = new TaskThumbnail(base.tasks.models[k]);
                 base.$el.find(".tasks_list").append(thumbnail.$el);
                 thumbnail.init(base.SmartBlocks);
             }
+
+            base.$el.find(".pagination").html('');
+            for (var i = 1; i <= base.page_count; i++) {
+                base.$el.find(".pagination").append('<a href="javascript:void(0);" class="' + (base.current_page == i ? 'selected' : '') +  '" data-page="' + i + '"><div></div></a>');
+            }
         },
         registerEvents: function () {
             var base = this;
+            base.$el.delegate('.pagination a', 'click', function () {
+                var elt = $(this);
+                var page = elt.attr('data-page');
+            });
         }
     });
 
